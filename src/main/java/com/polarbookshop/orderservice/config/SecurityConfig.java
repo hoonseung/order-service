53fa4a149f93879b0bad41e84d04e67e9e9d6937
lsh <tmdgns4029@naver.com>
2024-07-06 20:04:50 +0900

    cloud-config -> 쿠버네티스 kustomize 방법으로 사용자 지정 설정 적용

--- src/main/java/com/polarbookshop/orderservice/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/orderservice/config/SecurityConfig.java
@@ -14,7 +14,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain config(ServerHttpSecurity http) {
-        return http.authorizeExchange(exchange -> exchange.anyExchange().authenticated() )
+        return http.authorizeExchange(exchange -> exchange
+                        .pathMatchers("/actuator/**").permitAll()
+                        .anyExchange().authenticated() )
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt)
                 .requestCache(requestCache -> requestCache.requestCache(NoOpServerRequestCache.getInstance()))
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
